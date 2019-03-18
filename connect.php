9054171515dfcb4dd2f58c05bc7dc6b5909cda82
pcairns-rgu <47105288+pcairns-rgu@users.noreply.github.com>
2019-03-18 12:07:08 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,22 @@
-<?php
-define('DB_SERVER', 'CSDM-WEBDEV');
-define('DB_USERNAME', '1614072');
-define('DB_PASSWORD', '1614072');
-define('DB_DATABASE', 'db1614072_marvel');
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-// Check connection
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
+<?
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+foreach ($_SERVER as $key => $value) {
+ if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+ continue;
+ }
+ $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-echo "Connected successfully";
-
-mysqli_close($db);
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+if (!$link) {
+ echo "Error: Unable to connect to MySQL." . PHP_EOL;
+ echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+ echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+ exit;
+}
 ?>
